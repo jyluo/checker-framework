1ebf6ecab3bd4c86415380fa1efc8f4fa87cac2f
J Luo <j36luo@uwaterloo.ca>
2016-03-16 21:46:34 -0400

    Temp test expansion

--- checker/tests/units/Temp.java
+++ checker/tests/units/Temp.java
@@ -3,8 +3,33 @@ import org.checkerframework.checker.nullness.qual.*;
 
 import java.util.LinkedList;
 import java.util.List;
+//
+//// Test 1:
+//
+//class ClassA{}
+//
+//class SubClassA extends ClassA{}
+//
+//class ClassB< TT, HH extends TT >{
+//    public void mm( ClassB< TT, HH > input) {}
+//}
+//
+//class SubClassB extends ClassB < ClassA, @m SubClassA > {}
+//
+//class MM{
+//    public void mm2( ClassB< ClassA, ? > input) {}
+//    <TTT extends @Scalar ClassA> void mm3( ClassB< ClassA, TTT > input) {}
+//    
+//}
+//
+//// scalar explicit upper
+//// unknown implicit upper
+//// try this
 
-// Test 1:
+
+
+
+// Test 2:
 
 class blah{
     // lowerbound is declared to Meters, upperbound is declared as Length.
@@ -17,7 +42,9 @@ class blah{
     void m(){
         @UnitsBottom Object b = new @UnitsBottom Object();
         // b is UnitsBottom which is a subtype of meter, therefore accepted as a type argument?
-        //:: error: (assignment.type.incompatible)
-        b = declaredExplicitBounds(b);
+        b = this.<@UnitsBottom Object> declaredExplicitBounds(b);
+        
+        // if declared explicitly, it types, if inferred it doesn't. framework bug
     }
 }
+

e386369f7769fd8bff13d0fa7139a86858670199
Jeff Luo <j36luo@uwaterloo.ca>
2016-03-29 23:32:44 -0400

    Units Extensions Demo Changes
    - import statement updates
    - formatting cleanup

--- checker/examples/units-extension/Demo.java
+++ checker/examples/units-extension/Demo.java
@@ -1,31 +1,35 @@
 import org.checkerframework.checker.units.UnitsTools;
 import org.checkerframework.checker.units.qual.Prefix;
-import org.checkerframework.checker.units.qual.s;
-import qual.Frequency;
+import org.checkerframework.checker.units.qual.time.duration.s;
+
 import qual.Hz;
 import qual.kHz;
+import qual.Frequency;
 
 class UnitsExtensionDemo {
     @Hz int frq;
 
     void bad() {
-        // Error! Unqualified value assigned to a @Hz value.
+        // Error! Scalar value assigned to a @Hz value.
         //:: error: (assignment.type.incompatible)
         frq = 5;
 
-        // suppress all warnings issued by the units checker for the d1 assignment statement
+        // suppress all warnings issued by the units checker for the d1
+        // assignment statement
         @SuppressWarnings("units")
         @Hz int d1 = 9;
 
-        // specifically suppress warnings related to any frequency units for the d2 assigment statement
+        // specifically suppress warnings related to any frequency units for the
+        // d2 assigment statement
         @SuppressWarnings("frequency")
         @Hz int d2 = 10;
     }
 
-    // specifically suppresses warnings for the hz annotation for the toHz method
+    // specifically surpresses warnings for the hz annotation for the toHz
+    // method
     @SuppressWarnings("hz")
-    static @Hz int toHz(int hz) {
-        return hz;
+    static @Hz int toHz(int value) {
+        return value;
     }
 
     void good() {
@@ -37,13 +41,14 @@ class UnitsExtensionDemo {
 
     void auto(@s int time) {
         // The @Hz annotation is automatically added to the result
-        // of the division, because we provide class FrequencyRelations.
+        // of the division, as the rule is defined in FrequencyRelations class.
         frq = 99 / time;
     }
 
     public static void main(String[] args) {
         @Hz int hertz = toHz(20);
         @s int seconds = 5 * UnitsTools.s;
+        @s int otherSeconds = 7 * UnitsTools.s;
 
         @SuppressWarnings("units")
         @s(Prefix.milli) int millisec = 10;
@@ -57,11 +62,12 @@ class UnitsExtensionDemo {
         @kHz int resultkHz = kilohertz + 50 / millisec;
         System.out.println(resultkHz);
 
-        // this demonstrates the type hierarchy resolution: the common supertype of Hz and kHz is Frequency, so this statement will pass
-        @Frequency int okTernaryAssign = seconds > 10 ? hertz : kilohertz;
+        // the supertype of Hz and kHz is Frequency, so this statement will pass
+        @Frequency int okTernaryAssign = seconds > otherSeconds ? hertz : kilohertz;
 
-        // on the other hand, this statement expects the right hand side to be a Hz, so it will fail
+        // on the other hand, this statement expects the right hand side to be a
+        // Hz, so it will fail
         //:: error: (assignment.type.incompatible)
-        @Hz int badTernaryAssign = seconds > 10 ? hertz : kilohertz;
+        @Hz int badTernaryAssign = seconds > otherSeconds ? hertz : kilohertz;
     }
 }
--- checker/examples/units-extension/qual/Frequency.java
+++ checker/examples/units-extension/qual/Frequency.java
@@ -1,13 +1,10 @@
 package qual;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
+
 import org.checkerframework.checker.units.qual.UnitsRelations;
 import org.checkerframework.checker.units.qual.UnknownUnits;
-import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.*;
 
 /**
  * Units of frequency, such as hertz (@{@link Hz}).

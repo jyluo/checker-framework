3967159bcaf5c365db9671180966557762d7df9d
Jeff Luo <j36luo@uwaterloo.ca>
2019-02-20 16:19:08 -0500

    add tests for short

--- checker/tests/units/BoxedNumberTypes.java
+++ checker/tests/units/BoxedNumberTypes.java
@@ -92,11 +92,67 @@ public class BoxedNumberTypes {
         System.out.println(mB.hashCode());
         System.out.println(Byte.hashCode(mb));
 
+        mB.equals(mB);
+        // :: error: (comparison.unit.mismatch)
+        mB.equals(sB);
+
         mB.compareTo(mB);
         // :: error: (comparison.unit.mismatch)
         mB.compareTo(sB);
 
+        Byte.compare(mb, mb);
+        // :: error: (comparison.unit.mismatch)
+        Byte.compare(mb, sb);
+
         mI = Byte.toUnsignedInt(mb);
         mL = Byte.toUnsignedLong(mb);
+    }
+
+    void testShort() {
+        mS = Short.valueOf(ms);
+        // mS = new Short(ms);
+
+        mb = mS.byteValue();
+        ms = mS.shortValue();
+        mi = mS.intValue();
+        ml = mS.longValue();
+        mf = mS.floatValue();
+        md = mS.doubleValue();
+
+        // :: error: (assignment.type.incompatible)
+        sS = Short.valueOf(ms);
+        // :: error: (assignment.type.incompatible)
+        sb = mS.byteValue();
+        // :: error: (assignment.type.incompatible)
+        ss = mS.shortValue();
+        // :: error: (assignment.type.incompatible)
+        si = mS.intValue();
+        // :: error: (assignment.type.incompatible)
+        sl = mS.longValue();
+        // :: error: (assignment.type.incompatible)
+        sf = mS.floatValue();
+        // :: error: (assignment.type.incompatible)
+        sd = mS.doubleValue();
+
+        System.out.println(mS.toString());
+        System.out.println(Short.toString(ms));
+        System.out.println(mS.hashCode());
+        System.out.println(Short.hashCode(ms));
+
+        mS.equals(mS);
+        // :: error: (comparison.unit.mismatch)
+        mS.equals(sS);
+
+        mS.compareTo(mS);
+        // :: error: (comparison.unit.mismatch)
+        mS.compareTo(sS);
+
+        Short.compare(ms, ms);
+        // :: error: (comparison.unit.mismatch)
+        Short.compare(ms, ss);
+
+        ms = Short.reverseBytes(ms);
+        mI = Short.toUnsignedInt(ms);
+        mL = Short.toUnsignedLong(ms);
     }
 }
